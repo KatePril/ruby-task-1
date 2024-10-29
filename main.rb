d11020e66619c7d0a14a10a7a2c0089db3fdb860
KatePril <ket14pril14@ukr.net>
2024-10-29 21:56:39 +0200

    Add validation and add_student

---
+++ main.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+require 'date'
+
+puts Date.new(2030, 3, 14) > Date.today
--- student.rb
+++ student.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
+require 'date'
 
 class Student
-  @@all = []
+  @@students = []
   attr_reader :name
   attr_reader :surname
   attr_reader :date_of_birth
@@ -9,7 +10,47 @@ class Student
   def initialize(name, surname, date_of_birth)
     @name = name
     @surname = surname
+
+    if date_of_birth > Date.today
+      raise ArgumentError,'you entered a future date'
+    end
+
     @date_of_birth = date_of_birth
-    @@all << self
+
+    self.class.add_student(self)
+
   end
+
+  def eql?(other)
+    @name == other.name && @surname == other.surname && @date_of_birth == other.date_of_birth
+  end
+
+  def self.add_student(new_student)
+    unique = true
+    @@students.each { |student|
+
+      if student.eql? new_student
+
+        unique = false
+        break
+      end
+
+    }
+
+    if unique
+      @@students << new_student
+    end
+  end
+  def self.students
+    @@students
+  end
+
 end
+
+lucy = Student.new("Lucy", "Evans", Date.new(2000, 3, 14))
+lucy1 = Student.new("Lucy", "Evans", Date.new(2000, 3, 14))
+
+Student.add_student(Student.new("Emma", "Evans", Date.new(2010, 3, 14)))
+
+p lucy.eql? lucy1
+p Student.students
